2c69d40670bb7eab38a92991ec3e79a7a4af4e0a
RAGUNATH R <113915622+Ragu-123@users.noreply.github.com>
2025-10-02 14:37:34 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,38 @@
  
+from setuptools import setup, find_packages
+
+# Read the contents of your requirements.txt file
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='fastcap',
+    version='0.1.0',
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='FastCap: An Efficient Image Captioning Model',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/your-username/enhanced-fastcap',
+    project_urls={
+        "Bug Tracker": "https://github.com/your-username/enhanced-fastcap/issues",
+    },
+    license='GNU General Public License v3 (GPLv3)',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+    python_requires='>=3.8',
+    install_requires=requirements,
+    keywords='image-captioning, computer-vision, natural-language-processing, deep-learning, pytorch',
+)
